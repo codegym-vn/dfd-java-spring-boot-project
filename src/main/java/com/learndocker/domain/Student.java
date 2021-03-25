dc7b1a9d9f13e9da4d8014c6f30d8d8061a51906
Andy Le Bui <ble@shipserv.com>
2021-03-25 17:38:55 +0700

    Create base

---
+++ src/main/java/com/learndocker/domain/Student.java
@@ -0,0 +1,9 @@
+package com.learndocker.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+}
---
+++ src/main/java/com/learndocker/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.learndocker.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+}
